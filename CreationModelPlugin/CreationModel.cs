93e352e9f26abef8de19131bdb3e63b8bfb726c1
sergey leonov <redfortten@hotmail.com>
2022-02-14 22:08:43 +0300

    Добавьте файлы проекта.

---
+++ CreationModelPlugin/CreationModel.cs
@@ -0,0 +1,201 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+
+namespace CreationModelPlugin
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CreationModel : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            Level level1 = SelectLevel(doc, "Уровень 1");
+            Level level2 = SelectLevel(doc, "Уровень 2");
+            List<Wall> walls = CreateWalls(level1, level2, doc);
+            AddDoor(doc, level1, walls[0]);
+            AddWindow(doc, level1, walls[1]);
+            AddWindow(doc, level1, walls[2]);
+            AddWindow(doc, level1, walls[3]);
+            AddRoof(doc, level2, walls);
+
+            return Result.Succeeded;
+        }
+
+        private void AddRoof(Document doc, Level level2, List<Wall> walls)
+        {
+            RoofType roofType = new FilteredElementCollector(doc)
+                .OfClass(typeof(RoofType))
+                .OfType<RoofType>()
+                .Where(x => x.Name.Equals("Типовой - 400мм"))
+                .Where(x => x.FamilyName.Equals("Базовая крыша"))
+                .FirstOrDefault();
+
+            View view = new FilteredElementCollector(doc)
+                    .OfClass(typeof(View))
+                    .OfType<View>()
+                    .Where(x => x.Name.Equals("Уровень 2"))
+                    .FirstOrDefault();
+
+            double wallWidth = walls[0].Width;
+            double dt = wallWidth / 2;
+            double width = UnitUtils.ConvertToInternalUnits(10500, UnitTypeId.Millimeters);
+            double height = UnitUtils.ConvertToInternalUnits(5500, UnitTypeId.Millimeters);
+            double extrStart = -width / 2 - dt;
+            double extrEnd = width / 2 + dt;
+            double curveStart = -height / 2 - dt;
+            double curveEnd = +height / 2 + dt;
+
+            CurveArray curveArray = new CurveArray();
+            curveArray.Append(Line.CreateBound(new XYZ(0, curveStart, level2.Elevation), new XYZ(0, 0, level2.Elevation + 8)));
+            curveArray.Append(Line.CreateBound(new XYZ(0, 0, level2.Elevation + 8), new XYZ(0, curveEnd, level2.Elevation)));
+
+            Transaction transaction = new Transaction(doc, "Создание кровли");
+            transaction.Start();
+            ReferencePlane plane = doc.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(0, 0, 20), new XYZ(0, 20, 0), view);
+            ExtrusionRoof extrusionRoof = doc.Create.NewExtrusionRoof(curveArray, plane, level2, roofType, extrStart, extrEnd);
+            extrusionRoof.EaveCuts = EaveCutterType.TwoCutSquare;
+            transaction.Commit();
+        }
+
+        #region FootPrintRoof
+        //private void AddRoof(Document doc, Level level2, List<Wall> walls)
+        //{
+        //    RoofType roofType = new FilteredElementCollector(doc)
+        //        .OfClass(typeof(RoofType))
+        //        .OfType<RoofType>()
+        //        .Where(x => x.Name.Equals("Типовой - 400мм"))
+        //        .Where(x => x.FamilyName.Equals("Базовая крыша"))
+        //        .FirstOrDefault();
+
+        //    double wallWidth = walls[0].Width;
+        //    double dt = wallWidth / 2;
+        //    List<XYZ> points = new List<XYZ>();
+        //    points.Add(new XYZ(-dt, -dt, 0));
+        //    points.Add(new XYZ(dt, -dt, 0));
+        //    points.Add(new XYZ(dt, dt, 0));
+        //    points.Add(new XYZ(-dt, dt, 0));
+        //    points.Add(new XYZ(-dt, -dt, 0));
+
+
+        //    Application application = doc.Application;
+        //    CurveArray footprint = application.Create.NewCurveArray();
+        //    for (int i = 0; i < 4; i++)
+        //    {
+        //        LocationCurve curve = walls[i].Location as LocationCurve;
+        //        XYZ p1 = curve.Curve.GetEndPoint(0);
+        //        XYZ p2 = curve.Curve.GetEndPoint(1);
+        //        Line line = Line.CreateBound(p1 + points[i], p2 + points[i + 1]);
+        //        footprint.Append(line);
+        //    }
+
+        //    ModelCurveArray footPrintToModelCurveMapping = new ModelCurveArray();
+        //    FootPrintRoof footprintRoof = doc.Create.NewFootPrintRoof(footprint, level2, roofType, out footPrintToModelCurveMapping);
+        //    foreach(ModelCurve m in footPrintToModelCurveMapping)
+        //    {
+        //        footprintRoof.set_DefinesSlope(m, true);
+        //        footprintRoof.set_SlopeAngle(m, 0.5);
+        //    }
+        //}
+        #endregion
+
+        private void AddWindow(Document doc, Level level1, Wall wall)
+        {
+            FamilySymbol windowType = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_Windows)
+                .OfType<FamilySymbol>()
+                .Where(x => x.Name.Equals("0915 x 1830 мм"))
+                .Where(x => x.FamilyName.Equals("Фиксированные"))
+                .FirstOrDefault();
+
+            Transaction transaction = new Transaction(doc, "Создание окон");
+            transaction.Start();
+            LocationCurve hostCurve = wall.Location as LocationCurve;
+            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
+            XYZ point2 = hostCurve.Curve.GetEndPoint(1);
+            XYZ point = (point1 + point2) / 2;
+
+            if (!windowType.IsActive)
+                windowType.Activate();
+
+            FamilyInstance window = doc.Create.NewFamilyInstance(point, windowType, wall, level1, StructuralType.NonStructural);
+            double offsetHeigh = UnitUtils.ConvertToInternalUnits(500, UnitTypeId.Millimeters);
+            window.get_Parameter(BuiltInParameter.INSTANCE_SILL_HEIGHT_PARAM).Set(offsetHeigh);
+            transaction.Commit();
+        }
+
+        private void AddDoor(Document doc, Level level1, Wall wall)
+        {
+            FamilySymbol doorType = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_Doors)
+                .OfType<FamilySymbol>()
+                .Where(x => x.Name.Equals("0915 x 2134 мм"))
+                .Where(x => x.FamilyName.Equals("Одиночные-Щитовые"))
+                .FirstOrDefault();
+            Transaction transaction = new Transaction(doc, "Создание двери");
+            transaction.Start();
+            LocationCurve hostCurve = wall.Location as LocationCurve;
+            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
+            XYZ point2 = hostCurve.Curve.GetEndPoint(1);
+            XYZ point = (point1 + point2) / 2;
+
+            if (!doorType.IsActive)
+                doorType.Activate();
+
+            doc.Create.NewFamilyInstance(point, doorType, wall, level1, StructuralType.NonStructural);
+            transaction.Commit();
+        }
+
+        public List<Wall> CreateWalls(Level level1, Level level2, Document doc)
+        {
+            double width = UnitUtils.ConvertToInternalUnits(10000, UnitTypeId.Millimeters);
+            double depth = UnitUtils.ConvertToInternalUnits(5000, UnitTypeId.Millimeters);
+            double dx = width / 2;
+            double dy = depth / 2;
+
+            List<XYZ> points = new List<XYZ>();
+            List<Wall> walls = new List<Wall>();
+
+            Transaction transaction = new Transaction(doc, "Постронение стен");
+            transaction.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                points.Add(new XYZ(-dx, -dy, 0));
+                points.Add(new XYZ(dx, -dy, 0));
+                points.Add(new XYZ(dx, dy, 0));
+                points.Add(new XYZ(-dx, dy, 0));
+                points.Add(new XYZ(-dx, -dy, 0));
+                Line line = Line.CreateBound(points[i], points[i + 1]);
+                Wall wall = Wall.Create(doc, line, level1.Id, false);
+                wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
+                walls.Add(wall);
+            }
+            transaction.Commit();
+            return walls;
+        }
+
+        public Level SelectLevel(Document doc, string lavelName)
+        {
+            List<Level> listLevel = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+
+            Level level = listLevel
+                .Where(x => x.Name.Equals(lavelName))
+                .FirstOrDefault();
+
+            return level;
+        }
+    }
+}
